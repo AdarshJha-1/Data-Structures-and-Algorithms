585249500765f50d41f44da942591cdec9f66b66
Adarsh Jha <adarsh2005j@gmail.com>
2024-08-22 14:54:45 +0530

    1672 sol

--- src/arrays/RichestCustomerWealth1672.java
+++ src/arrays/RichestCustomerWealth1672.java
@@ -3,7 +3,6 @@ package arrays;
 public class RichestCustomerWealth1672 {
     public int maximumWealth(int[][] accounts) {
         int maxWhealth = 0;
-
         for(int i = 0; i < accounts.length; i++) {
             int sum = 0;
             for(int j = 0; j < accounts[i].length; j++){
